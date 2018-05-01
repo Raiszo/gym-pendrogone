9ebb08b0a79518a53ebe470d84ff0d305d31a1c5
Raiszo <E05.sek@gmail.com>
2018-05-01 11:49:56 -0500

    :dagger: working simple 2d drone environment

--- gym_pendrogone/envs/drone2d_env.py
+++ gym_pendrogone/envs/drone2d_env.py
@@ -15,6 +15,7 @@ class Drone2dEnv(gym.Env):
         self.mass = 0.5 #: [kg] mass
         self.Ixx = 0.00025
         self.arm_length = 0.086 # [m]
+        self.arm_width = 0.02 # [m]
         self.height = 0.02 # [m]
 
         # max and min force for each motor
@@ -85,29 +86,51 @@ class Drone2dEnv(gym.Env):
     
     def render(self, mode='human', close=False):
         from gym.envs.classic_control import rendering
-        screen_width = 600
-        screen_height = 400
-
-        arm_length = 300
-        arm_width = 50
-        propeller_rad = 20
-        world_width = 2
-        scale = 1
+        screen_width = 500
+        screen_height = 500
 
         x,z,phi = self.state[0:3].tolist()
 
+        T = np.array([[np.cos(phi), -np.sin(phi), x],
+                      [np.sin(phi), np.cos(phi), z],
+                      [0, 0, 1]])
+
+        bodyFrame = np.array([[self.arm_length, 0 , 1],
+                              [-self.arm_length, 0 , 1]])
+
+        worldFrame = T.dot(bodyFrame.T)[:-1,:]
+        t1_xy = worldFrame[:,0].tolist()
+        t2_xy = worldFrame[:,1].tolist()
+        
         if self.viewer is None:
             self.viewer = rendering.Viewer(screen_width, screen_height)
-            self.viewer.set_bounds(-2.2,2.2,-2.2,2.2)
+            self.viewer.set_bounds(-1,1,-1,1)
             
-            l,r,t,b = -arm_length, arm_length, arm_width, -arm_width
+            l,r,t,b = -self.arm_length, self.arm_length, self.arm_width, -self.arm_width
+            self.frame_trans = rendering.Transform(rotation=phi, translation=(x,z))
             frame = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-            self.frametrans = rendering.Transform(rotation=phi, translation=(x,z))
+            frame.set_color(0, .8, .8)
+            frame.add_attr(self.frame_trans)
+            self.viewer.add_geom(frame)
 
+            self.t1_trans = rendering.Transform(translation=t1_xy)
+            thruster1 = self.viewer.draw_circle(.04)
+            thruster1.set_color(.8, .8, 0)
+            thruster1.add_attr(self.t1_trans)
+            self.viewer.add_geom(thruster1)
 
+            self.t2_trans = rendering.Transform(translation=t2_xy)
+            thruster2 = self.viewer.draw_circle(.04)
+            thruster2.set_color(.8, .8, 0)
+            thruster2.add_attr(self.t2_trans)
+            self.viewer.add_geom(thruster2)
+
+        self.frame_trans.set_translation(x,z)
+        self.frame_trans.set_rotation(phi)
         
-        frame = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
-        self.viewer.add_geom(frame)
+        self.t1_trans.set_translation(t1_xy[0], t1_xy[1])
+        self.t2_trans.set_translation(t2_xy[0], t2_xy[1])
+        
         return self.viewer.render(return_rgb_array = mode=='rgb_array')
 
     """
